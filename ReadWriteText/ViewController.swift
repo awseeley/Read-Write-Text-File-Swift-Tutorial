419bf25628d5e5ba99591e4d453b8e370b9764fb
Andrew Seeley <anseeley@au1.ibm.com>
2016-04-07 18:17:16 +1000

    Read Write Text File Swift Tutorial
    Read Write Text File Swift Tutorial

--- ReadWriteText/ViewController.swift
+++ ReadWriteText/ViewController.swift
@@ -13,6 +13,48 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Save data to file
+        let fileName = "Test"
+        let DocumentDirURL = try! NSFileManager.defaultManager().URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true)
+        
+        let fileURL = DocumentDirURL.URLByAppendingPathComponent(fileName).URLByAppendingPathExtension("txt")
+        print("FilePath: \(fileURL.path)")
+        
+        let writeString = "Write this text to the fileURL as text in iOS using Swift"
+        do {
+            // Write to the file
+            try writeString.writeToURL(fileURL, atomically: true, encoding: NSUTF8StringEncoding)
+        } catch let error as NSError {
+            print("Failed writing to URL: \(fileURL), Error: " + error.localizedDescription)
+        }
+     
+        var readString = "" // Used to store the file contents
+        do {
+            // Read the file contents
+            readString = try String(contentsOfURL: fileURL)
+        } catch let error as NSError {
+            print("Failed reading from URL: \(fileURL), Error: " + error.localizedDescription)
+        }
+        print("File Text: \(readString)")
+        
+        /*** Read from project txt file ***/
+        
+        // File location
+        let fileURLProject = NSBundle.mainBundle().pathForResource("ProjectTextFile", ofType: "txt")
+        // Read from the file
+        var readStringProject = ""
+        do {
+            readStringProject = try String(contentsOfFile: fileURLProject!, encoding: NSUTF8StringEncoding)
+        } catch let error as NSError {
+             print("Failed reading from URL: \(fileURL), Error: " + error.localizedDescription)
+        }
+
+        print(readStringProject)
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
